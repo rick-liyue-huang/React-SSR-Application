24cce4ed71b52c1cbf2fa61df676bdff5d52f40a
liyue <rick.liyue.huang@gmail.com>
2019-11-19 22:16:28 +1100

    feat: create the initial structure

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(
+    `<html>
+      <head>
+        <title>hello</title>
+      </head>
+      <body>
+        <h1>first lesson</h1>
+        <p>hello world</p>
+      </body>
+     </html>`
+  )
+});
+
+const server = app.listen(3000);
